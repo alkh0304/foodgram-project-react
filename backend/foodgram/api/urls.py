09c4c7f08adf2a10fab2988a82986962a544fe31
alkh0304 <alkh0304@yandex.ru>
2022-06-17 19:55:08 +0300

    user creation

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -8,7 +8,6 @@ router_v1.register('tags', views.TagViewset, basename='tag')
 router_v1.register('ingredients', views.IngredientViewset,
                    basename='ingredient')
 router_v1.register('recipes', views.RecipeViewset, basename='recipe')
-router_v1.register('users', views.UserViewSet, basename='users')
 router_v1.register('users/(?P<id>[^/.]+)/subscribe', views.SubscriptionViewSet,
                    basename='subscription')
 
