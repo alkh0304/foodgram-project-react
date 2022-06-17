f68eea3fe1248d1b77264a2facd917dac62411d3
alkh0304 <alkh0304@yandex.ru>
2022-06-17 21:48:12 +0300

    user creation

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .utils import CustomUserManager
-
 
 class CustomUser(AbstractUser):
     email = models.EmailField(
@@ -13,19 +11,9 @@ class CustomUser(AbstractUser):
     )
     first_name = models.CharField('Имя', max_length=30, blank=True)
     last_name = models.CharField('Фамилия', max_length=150, blank=True)
-    bio = models.CharField('Биография', max_length=200, blank=True,
-                           null=True, default=1)
-    confirmation_code = models.CharField(
-        'Код подтверждения', blank=True, null=True, max_length=128, default=1
-    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-
-    objects = CustomUserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
