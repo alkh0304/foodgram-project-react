4ba28aaaee91e5a053e28c0a79f845e44ce5d2ff
alkh0304 <alkh0304@yandex.ru>
2022-06-17 21:21:20 +0300

    user creation

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -20,6 +20,17 @@ class UserSerializer(UserSerializer):
             'last_name'
         )
 
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     """Сериализатор подписки."""
