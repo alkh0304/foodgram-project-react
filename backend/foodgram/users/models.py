ee77a8198529c0e0f159a3b076b9d9a9296894a5
alkh0304 <alkh0304@yandex.ru>
2022-06-17 20:35:33 +0300

    user creation

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-from utils import CustomUserManager
+from .utils import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
