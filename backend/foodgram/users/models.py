b951026b4606d812b2569334e71578e77798603c
alkh0304 <alkh0304@yandex.ru>
2022-06-17 20:44:39 +0300

    user creation

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from .utils import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     email = models.EmailField(
         unique=True,
         max_length=254,
