b0bdd1e41b5a03e0b65a8cb25fabefbd40b29c69
alkh0304 <alkh0304@yandex.ru>
2022-06-18 13:43:45 +0300

    redoc

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,10 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
