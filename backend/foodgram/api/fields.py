f54d93f92711d26b9f77c11439bb58946ee89b60
alkh0304 <alkh0304@yandex.ru>
2022-06-19 09:06:42 +0300

    imagee

--- backend/foodgram/api/fields.py
+++ backend/foodgram/api/fields.py
@@ -1,30 +1,49 @@
-from base64 import b64decode
-from imghdr import what
-from uuid import uuid4
+import base64
+import imghdr
+import uuid
 
 from django.core.files.base import ContentFile
 from rest_framework.serializers import ImageField
-from six import string_types
-
-WRONG_IMAGE_TYPE = "Кривой формат файла картинки"
-BASE64 = ";base64,"
 
 
 class Base64ImageField(ImageField):
+    """
+    A Django REST framework field for handling image-uploads through raw post
+    data.
+    It uses base64 for encoding and decoding the contents of the file.
+    Heavily based on
+    https://github.com/tomchristie/django-rest-framework/pull/1268
+    Updated for Django REST framework 3.
+    """
+
     def to_internal_value(self, data):
-        if isinstance(data, string_types):
-            if "data:" in data and BASE64 in data:
-                header, data = data.split(BASE64)
+        # Check if this is a base64 string
+        if isinstance(data, str):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
             try:
-                decoded_file = b64decode(data)
+                decoded_file = base64.b64decode(data)
             except TypeError:
-                self.fail(WRONG_IMAGE_TYPE)
-            file_name = str(uuid4())[:12]
+                self.fail('invalid_image')
+
+            # Generate file name:
+            # 12 characters are more than enough.
+            file_name = str(uuid.uuid4())[:12]
+            # Get the file name extension:
             file_extension = self.get_file_extension(file_name, decoded_file)
-            complete_file_name = "%s.%s" % (file_name, file_extension)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
             data = ContentFile(decoded_file, name=complete_file_name)
-        return super().to_internal_value(data)
+
+        return super(Base64ImageField, self).to_internal_value(data)
 
     def get_file_extension(self, file_name, decoded_file):
-        extension = what(file_name, decoded_file)
+
+        extension = imghdr.what(file_name, decoded_file)
+
         return "jpg" if extension == "jpeg" else extension
