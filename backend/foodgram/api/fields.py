d02fdcb5a998761eb2c5ab03c444ca38a2911b70
alkh0304 <alkh0304@yandex.ru>
2022-06-19 09:23:09 +0300

    image

--- backend/foodgram/api/fields.py
+++ backend/foodgram/api/fields.py
@@ -1,49 +1,24 @@
 import base64
-import imghdr
 import uuid
 
 from django.core.files.base import ContentFile
-from rest_framework.serializers import ImageField
+from rest_framework.serializers import ImageField, ValidationError
+
+IMAGE_UUID4_NAME = '{}.{}'
+IMAGE64ERROR = 'ошибка'
 
 
 class Base64ImageField(ImageField):
-    """
-    A Django REST framework field for handling image-uploads through raw post
-    data.
-    It uses base64 for encoding and decoding the contents of the file.
-    Heavily based on
-    https://github.com/tomchristie/django-rest-framework/pull/1268
-    Updated for Django REST framework 3.
-    """
+    """Поле для декодирования из 64 бит в jpg."""
 
     def to_internal_value(self, data):
-        # Check if this is a base64 string
-        if isinstance(data, str):
-            # Check if the base64 string is in the "data:" format
-            if 'data:' in data and ';base64,' in data:
-                # Break out the header from the base64 content
-                header, data = data.split(';base64,')
-
-            # Try to decode the file. Return validation error if it fails.
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-
-            # Generate file name:
-            # 12 characters are more than enough.
-            file_name = str(uuid.uuid4())[:12]
-            # Get the file name extension:
-            file_extension = self.get_file_extension(file_name, decoded_file)
-
-            complete_file_name = "%s.%s" % (file_name, file_extension, )
-
-            data = ContentFile(decoded_file, name=complete_file_name)
-
-        return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-
-        extension = imghdr.what(file_name, decoded_file)
-
-        return "jpg" if extension == "jpeg" else extension
+        """Декодирование и сохранение."""
+        try:
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(
+                base64.b64decode(imgstr),
+                name=IMAGE_UUID4_NAME.format(str(uuid.uuid4()), ext))
+        except ValueError:
+            raise ValidationError(IMAGE64ERROR)
+        return data
